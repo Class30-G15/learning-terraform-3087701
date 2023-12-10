648171acc516d63555a618a36bbaa73b8167db65
Kuchambiatud <107870163+Kuchambiatud@users.noreply.github.com>
2023-12-10 10:34:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,54 +15,43 @@ data "aws_ami" "app_ami" {
 }
 
 data "aws_vpc" "default" {
-default = true
+  default = true
 }
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
-    Name = "HelloWorld"
+    name = "learning Terraform"
   }
 }
 
 resource "aws_security_group" "blog" {
-name        = "blog"
-description = "Allow http in. Allow everything out"
+  name        = "blog"
+  description = "Allow http in. Allow everything out"
 
-vpc_id = data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
-type = "ingress"
-from_port = 80
-to_port   = 80
-protocol  = "tcp"
-cidr_blocks = {"0.0.0.0/0"}
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
-security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
-type = "ingress"
-from_port = 443
-to_port   = 443
-protocol  = "tcp"
-cidr_blocks = {"0.0.0.0/0"}
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
 
-security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
-
-resource "aws_security_group_rule" "blog_http_in" {
-type = "engress"
-from_port = 0
-to_port   = 0
-protocol  = "-1"
-cidr_blocks = {"0.0.0.0/0"}
-
-security_group_id = aws_security_group.blog.id
-}
-
